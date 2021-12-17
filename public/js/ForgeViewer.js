a4ff6b633d5c9ebf676c0fc010894a895a6ba943
danielkiko <danielkiko@yandex.ru>
2021-12-17 18:23:11 +0300

    скрытие второго нозла

--- public/js/ForgeViewer.js
+++ public/js/ForgeViewer.js
@@ -1,6 +1,7 @@
 var viewer;
 var documentId;
 var hotend = true;
+
 launchViewer()
 function launchViewer() {
   var options = {
@@ -8,13 +9,15 @@ function launchViewer() {
     getAccessToken: getForgeToken
   };
 
-  
+
 
   Autodesk.Viewing.Initializer(options, () => {
-    viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'), { extensions: [ 'Autodesk.DocumentBrowser'] });
+    viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'), { extensions: ['Autodesk.DocumentBrowser'] });
     viewer.start();
-    documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6aG90ZW5kL0UzRF9WNl9Ib3RlbmRfMV83NW1tX3VuaXZlcnNhbC4xMTVjNTgzNS1iMDUzLTRlY2MtOGVhZC1mNmEzYTQxYmRmYmUuZjNkJTIwdjUuZjNk'//'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6M2RwcmludGJ1Y2svRW5kZXIlMjAzJTIwJTIwZW4lMjAzZC5mM2QlMjB2MS5mM2Q';
+    documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6aG90ZW5kL0UzRF9WNl9Ib3RlbmRfMV83NW1tX3VuaXZlcnNhbC4xMTVjNTgzNS1iMDUzLTRlY2MtOGVhZC1mNmEzYTQxYmRmYmUuZjNkJTIwdjUuZjNk'
+    // console.log(secondnozle);//'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6M2RwcmludGJ1Y2svRW5kZXIlMjAzJTIwJTIwZW4lMjAzZC5mM2QlMjB2MS5mM2Q';
     Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
+    
   });
 }
 
@@ -22,6 +25,25 @@ function onDocumentLoadSuccess(doc) {
   var viewables = doc.getRoot().getDefaultGeometry();
   viewer.loadDocumentNode(doc, viewables).then(i => {
     // documented loaded, any action?
+    viewer.addEventListener(Autodesk.Viewing.GEOMETRY_LOADED_EVENT, function () {viewer.impl.visibilityManager.setNodeOff(13, true);});
+
+    console.log(viewer.model.getDocumentNode());
+    viewer.addEventListener(Autodesk.Viewing.SELECTION_CHANGED_EVENT, function () {
+      const tree = viewer.model.getInstanceTree();
+      if (tree) { // Could be null if the tree hasn't been loaded yet
+        const selectedIds = viewer.getSelection();
+        for (const dbId of selectedIds) {
+          const fragIds = [];
+          tree.enumNodeFragments(
+            dbId,
+            function (fragId) { fragIds.push(fragId); },
+            false
+          );
+          console.log('dbId:', dbId, 'fragIds:', fragIds);
+          
+        }
+      }
+    });
   });
 }
 
@@ -37,17 +59,16 @@ function getForgeToken(callback) {
   });
 }
 
-function Changeurn()
-  {
-    if (hotend) {
+function Changeurn() {
+  if (hotend) {
     documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6aG90ZW5kL0UzRF9WNl9Ib3RlbmRfMV83NW1tX3VuaXZlcnNhbC4xMTVjNTgzNS1iMDUzLTRlY2MtOGVhZC1mNmEzYTQxYmRmYmUuZjNkJTIwdjUuZjNk';
     Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
     document.getElementById("change").innerHTML = "Поменять на хотэнд";
     hotend = false;
-    }
-    else {
-      documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6aG90ZW5kL19FM0RfVjZfVk9MQ0FOT19Ib3RlbmRfMV83NW1tLmJmYTdlZTgzLTcxMjktNDgwNy05NDdiLTBlNDJkYzQxNzRiYy5mM2QlMjB2Ni5mM2Q';//'вулкан';
-      Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
-      document.getElementById("change").innerHTML = "Поменять на принтер";
-    }
   }
+  else {
+    documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6aG90ZW5kL19FM0RfVjZfVk9MQ0FOT19Ib3RlbmRfMV83NW1tLmJmYTdlZTgzLTcxMjktNDgwNy05NDdiLTBlNDJkYzQxNzRiYy5mM2QlMjB2Ni5mM2Q';//'вулкан';
+    Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
+    document.getElementById("change").innerHTML = "Поменять на принтер";
+  }
+}
